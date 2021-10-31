e09b8515d1c7f121e27928e6f89bcc6a1ad111c8
Fabrice-K <fkakchantichi@gmail.com>
2021-10-31 15:02:14 +0100

    First commit

---
+++ src/test/java/com/example/gradleinitialisation/GradleInitialisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradleinitialisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleInitialisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
